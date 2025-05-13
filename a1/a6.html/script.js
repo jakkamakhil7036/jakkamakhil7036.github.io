75d19f8e0245e77806d30ee13e02e548107ebfb7
jakkamakhil7036 <akhiljakkam12@gmail.com>
2025-05-13 19:14:03 +0530

    update

---
+++ a1/a6.html/script.js
@@ -0,0 +1,11 @@
+function showCredentials()
+{
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;
+
+
+    document.getElementById('output').innerHTML=`
+    <p>Email: ${email}</p>
+    <p>Password: ${password}</p>
+    `;
+}
